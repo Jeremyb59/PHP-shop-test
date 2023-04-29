6698625d9584055aa8bdc0d72c8d20e23543f288
Jérémy Bouillet <88490357+Jeremyb59@users.noreply.github.com>
2023-04-30 00:33:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
-    //入口文件
-    //引入核心启动类
     include './framework/core/Framework.class.php';
-    // $app = new Framework();
-    // $app->run();
-    // 改进上述方法
     Framework::run();
-    
-    
