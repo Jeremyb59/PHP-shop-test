608c37230fc99ebf827a05ff3d88d13c63262d35
Jérémy Bouillet <88490357+Jeremyb59@users.noreply.github.com>
2023-05-01 18:09:51 +0200

    Update input.php

--- framework/helpers/input.php
+++ framework/helpers/input.php
@@ -1,5 +1,4 @@
 <?php
-//批量实体转义
 function deepspecialchars($data){
 	if (empty($data)) {
 		return $data;
@@ -7,7 +6,6 @@ function deepspecialchars($data){
 	return is_array($data) ? array_map('deepspecialchars',$data) : htmlspecialchars($data);
 }
 
-//批量单引号转义
 function deepslashes($data){
 	if (empty($data)) {
 		return $data;
